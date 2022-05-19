92d0f4e3f463effb2effe0892c41815ce979b316
AlexOnWax <alexandre.barbet@hotmail.com>
2022-05-19 10:06:16 +0200

    maj

--- script.js
+++ script.js
@@ -1,7 +1,26 @@
-const form = document.getElementById('form')
-const name = document.getElementById('name')
+const form = document.getElementById('formulaire')
+const firstName = document.getElementById('name')
 const lastName = document.getElementById('last-name')
 const eMail = document.getElementById('email')
 const password = document.getElementById('Password')
 const btn =document.getElementById('btn')
 
+const nameValue = firstName.value.trim()
+
+const lastNameValue = lastName.value.trim()
+
+const eMailValue = eMail.value.trim()
+
+const passwordValue = password.value.trim()
+
+
+form.addEventListener('submit', function(e){
+
+    e.preventDefault();
+    console.log("form ok");
+if(nameValue==="") {
+console.log('erreur');
+}else{
+    console.log('succes');
+}
+});
