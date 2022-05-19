e65c4ce8854362e3b75234ca0c840821a74e51e1
AlexOnWax <alexandre.barbet@hotmail.com>
2022-05-19 09:28:01 +0200

    creation of the js

---
+++ script.js
@@ -0,0 +1,7 @@
+const form = document.getElementById('form')
+const name = document.getElementById('name')
+const lastName = document.getElementById('last-name')
+const eMail = document.getElementById('email')
+const password = document.getElementById('Password')
+const btn =document.getElementById('btn')
+
