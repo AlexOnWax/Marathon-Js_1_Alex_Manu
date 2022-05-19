d3e77da5d2bf82a16a6f7ab3e33f95e3e39ff47d
AlexOnWax <alexandre.barbet@hotmail.com>
2022-05-19 16:37:49 +0200

    maj form

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ const eMail = document.getElementById("email");
 const password = document.getElementById("Password");
 const btn = document.getElementById("btn");
 const paragraphe = document.getElementById("text-explication");
-const nameValue = firstName.value.trim();
+const nameValue = firstName.value;
 const inputs = document.querySelectorAll(".input");
-const lastNameValue = lastName.value.trim();
+const lastNameValue = lastName.value;
 const error1 =document.querySelector(".error1")
 const errors = document.querySelectorAll(".invisible");
-const eMailValue = eMail.value.trim();
+const eMailValue = eMail.value;
 
-const passwordValue = password.value.trim();
+const passwordValue = password.value;
 
 
 
@@ -27,13 +27,12 @@ const passwordValue = password.value.trim();
 //     if (inputs[i].value === "") {
 //       span = document.createElement("SPAN");
 //       span = document.createTextNode(`${inputs[i].id} cannot be empty`);
-//       errors[i].appendChild(span)
+//       error1.appendChild(span);
 //     }
 //   });
 
 // }
 
-// const nameInput = document.querySelector('input');
 formulaire.addEventListener("submit", function (e) {
   
   e.preventDefault();
@@ -42,9 +41,6 @@ formulaire.addEventListener("submit", function (e) {
     firstName.setCustomValidity('');
     firstName.checkValidity();
   });
-  
-
-
 firstName.addEventListener('invalid', () => {
   if(firstName.value === '') {
     firstName.setCustomValidity('Enter your username!');
@@ -52,6 +48,43 @@ firstName.addEventListener('invalid', () => {
     firstName.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
   }
 });
+
+lastName.addEventListener('input', () => {
+  lastName.setCustomValidity('');
+  lastName.checkValidity();
+});
+lastName.addEventListener('invalid', () => {
+if(lastName.value === '') {
+  lastName.setCustomValidity('Enter your username!');
+} else {
+  lastName.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
+}
+});
+
+eMail.addEventListener('input', () => {
+  eMail.setCustomValidity('');
+  eMail.checkValidity();
+});
+eMail.addEventListener('invalid', () => {
+if(eMail.value === '') {
+  eMail.setCustomValidity('Enter your username!');
+} else {
+  eMail.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
+}
+});
+
+password.addEventListener('input', () => {
+  password.setCustomValidity('');
+  password.checkValidity();
+});
+password.addEventListener('invalid', () => {
+if(password.value === '') {
+  password.setCustomValidity('Enter your username!');
+} else {
+  password.setCustomValidity('Usernames can only contain upper and lowercase letters. Try again!');
+}
+});
+
 
 })
 
