da78ea0b70d1bdb07921889d09b088facf6751a8
AlexOnWax <alexandre.barbet@hotmail.com>
2022-05-20 13:56:22 +0200

    project near to be finished

--- script.js
+++ script.js
@@ -8,63 +8,44 @@ const paragraphe = document.getElementById("text-explication");
 const nameValue = firstName.value;
 const inputs = document.querySelectorAll(".input");
 const lastNameValue = lastName.value;
-const error1 =document.querySelector(".error1")
+const error1 = document.querySelector(".error1");
 const errors = document.querySelectorAll(".invisible");
 const eMailValue = eMail.value;
 const passwordValue = password.value;
 
-
 function inputValidation() {
-  
-  var i=-1
+  var i = -1;
   inputs.forEach((input) => {
-    i++
-    input.addEventListener('input', () => {
-    input.setCustomValidity('');
-    input.checkValidity();
-  });
+    i++;
+    input.addEventListener("input", () => {
+      input.setCustomValidity("");
+      input.checkValidity();
+    });
 
-     if(input.validity.valueMissing) {
-    const message = document.createTextNode(`${input.id} cannot be empty`)
-    errors[i].replaceChildren(message)
-      errors[i].classList.add('help')
-      input.classList.add('error')
-      input.style.background="url('images/icon-error.svg') no-repeat right 10px center "
-
-     } else if (input.validity.patternMismatch) {
-    const message = document.createTextNode(`${input.id} syntax`)
-    errors[i].replaceChildren(message)
-    errors[i].classList.add('help')
-    input.classList.add('error')
-
-     }else if (input.validity.typeMismatch) {
-    const message = document.createTextNode(`${input.id} Looks like this is not an email`)
-    errors[i].replaceChildren(message)
-    errors[i].classList.add('help')
-    input.classList.add('error')
-
-     } else {
-      const message = document.createTextNode('')
-      errors[i].replaceChildren(message)
-      errors[i].classList.add('help')
-      input.classList.remove('error')
-
-     }
-
-     
-    
+    if (input.validity.valueMissing) {
+      const message = document.createTextNode(`${input.id} cannot be empty`);
+      errors[i].replaceChildren(message);
+      errors[i].classList.add("help");
+      input.classList.add("error");
+      input.style.background =
+        "url('images/icon-error.svg') no-repeat right 10px center ";
+    } else if (input.validity.typeMismatch) {
+      const message = document.createTextNode(
+        `Looks like this is not an email`
+      );
+      errors[i].replaceChildren(message);
+      errors[i].classList.add("help");
+      input.classList.add("error");
+    } else {
+      const message = document.createTextNode("");
+      errors[i].replaceChildren(message);
+      errors[i].classList.add("help");
+      input.classList.remove("error");
     }
-    )
-
+  });
 }
-
 
 formulaire.addEventListener("submit", function (event) {
   event.preventDefault();
   inputValidation();
- 
-  
-  })
-    
-
-
+});
